1318559d6bec8090ff111de4938db24285c7ccc4
Sergei Bashkintcev <jestersong@mail.ru>
2023-10-02 09:59:49 +0400

    Successfully!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Still fighting with GitHub!");
+        System.out.println("I suppose I defeated GitHub!!!!!!");
     }
 }
